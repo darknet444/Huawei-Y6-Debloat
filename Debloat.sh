d0da0aa9c9054dd165be4cb41748483486191659
DarkNet <80594186+darknet444@users.noreply.github.com>
2022-08-24 18:16:59 +0200

    Create Debloat.sh

---
+++ Debloat.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+#########################################################
+#		                  LIST DEVICE 			                #
+#########################################################
+adb devices 
+
+########################################################
+#                  UNINSTALL APPS		                   #
+########################################################
+adb shell pm uninstall --user 0 com.android.backupconfirm
+adb shell pm uninstall --user 0 com.android.calculator2
+adb shell pm uninstall --user 0 com.android.calendar
+adb shell pm uninstall --user 0 com.android.calllogbackup
+adb shell pm uninstall --user 0 com.android.chrome
+adb shell pm uninstall --user 0 com.android.deskclock 
+adb shell pm uninstall --user 0 com.android.dreams.basic 
+adb shell pm uninstall --user 0 com.android.dreams.phototable
+adb shell pm uninstall --user 0 com.android.email
+adb shell pm uninstall --user 0 com.android.galaxy4
+adb shell pm uninstall --user 0 com.android.gallery3d
+adb shell pm uninstall --user 0 com.android.htmlviewer
+adb shell pm uninstall --user 0 com.android.hwmirror
+adb shell pm uninstall --user 0 com.android.inputdevices
+adb shell pm uninstall --user 0 com.android.managedprovisioning
+adb shell pm uninstall --user 0 com.android.mediacenter
+adb shell pm uninstall --user 0 com.android.noisefield
+adb shell pm uninstall --user 0 com.android.onetimeinitializer
+adb shell pm uninstall --user 0 com.android.pacprocessor
+adb shell pm uninstall --user 0 com.android.partnerbrowsercustomizations.tmobile
+adb shell pm uninstall --user 0 com.android.phasebeam
+adb shell pm uninstall --user 0 com.android.printspooler
+adb shell pm uninstall --user 0 com.android.protips
+adb shell pm uninstall --user 0 com.android.providers.calendar
+adb shell pm uninstall --user 0 com.android.providers.partnerbookmarks
+adb shell pm uninstall --user 0 com.android.proxyhandler
+adb shell pm uninstall --user 0 com.android.soundrecorder
+adb shell pm uninstall --user 0 com.android.stk
+adb shell pm uninstall --user 0 com.android.vending
+adb shell pm uninstall --user 0 com.android.wallpaper.holospiral
+adb shell pm uninstall --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall --user 0 com.android.wallpapercropper
+adb shell pm uninstall --user 0 com.example.android.notepad
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.facebook.katana
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.factory.factoryservice
+adb shell pm uninstall --user 0 com.gameloft.android.GloftPDMF
+adb shell pm uninstall --user 0 com.gameloft.android.GloftSCCA
+adb shell pm uninstall --user 0 com.google.android.apps.docs 
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.docs
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.sheets
+adb shell pm uninstall --user 0 com.google.android.apps.docs.editors.slides
+adb shell pm uninstall --user 0 com.google.android.apps.maps
+adb shell pm uninstall --user 0 com.google.android.apps.photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall --user 0 com.google.android.backuptransport
+adb shell pm uninstall --user 0 com.google.android.configupdater
+adb shell pm uninstall --user 0 com.google.android.feedback
+adb shell pm uninstall --user 0 com.google.android.gm
+adb shell pm uninstall --user 0 com.google.android.gms
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall --user 0 com.google.android.gsf
+adb shell pm uninstall --user 0 com.google.android.gsf.login
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.android.music
+adb shell pm uninstall --user 0 com.google.android.onetimeinitializer
+adb shell pm uninstall --user 0 com.google.android.setupwizard
+adb shell pm uninstall --user 0 com.google.android.syncadapters.calendar
+adb shell pm uninstall --user 0 com.google.android.syncadapters.contacts
+adb shell pm uninstall --user 0 com.google.android.tts
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.hp.android.printservice
+adb shell pm uninstall --user 0 com.huawei.HwMultiScreenShot
+adb shell pm uninstall --user 0 com.huawei.KoBackup
+adb shell pm uninstall --user 0 com.huawei.android.FMRadio
+adb shell pm uninstall --user 0 com.huawei.android.FloatTasks
+adb shell pm uninstall --user 0 com.huawei.android.hwaps
+adb shell pm uninstall --user 0 com.huawei.android.hwouc
+adb shell pm uninstall --user 0 com.huawei.android.hwpay
+adb shell pm uninstall --user 0 com.huawei.android.temperaturemonitor
+adb shell pm uninstall --user 0 com.huawei.android.thememanager
+adb shell pm uninstall --user 0 com.huawei.android.totemweather
+adb shell pm uninstall --user 0 com.huawei.appmarket
+adb shell pm uninstall --user 0 com.huawei.compass
+adb shell pm uninstall --user 0 com.huawei.gamebox.global
+adb shell pm uninstall --user 0 com.huawei.hidisk
+adb shell pm uninstall --user 0 com.huawei.hwid
+adb shell pm uninstall --user 0 com.huawei.hwstartupguide
+adb shell pm uninstall --user 0 com.huawei.hwvplayer
+adb shell pm uninstall --user 0 com.huawei.motionservice
+adb shell pm uninstall --user 0 com.huawei.phoneservice
+adb shell pm uninstall --user 0 com.huawei.screenrecorder
+adb shell pm uninstall --user 0 com.huawei.vassistant
+adb shell pm uninstall --user 0 com.instagram.android
+adb shell pm uninstall --user 0 com.nuance.swype.emui
+adb shell pm uninstall --user 0 com.qeexo.smartshot
+adb shell pm uninstall --user 0 com.runtastic.android.results.lite
+adb shell pm uninstall --user 0 org.simalliance.openmobileapi.service
+adb shell pm uninstall --user 0 se.dirac.acs
+#adb shell pm uninstall --user 0 com.huawei.android.launcher #Default HuaweiLauncher
+#adb shell pm uninstall --user 0 com.google.android.inputmethod.latin #Google Keyboard
+#adb shell pm uninstall --user 0 com.huawei.systemmanager #Huawei System Cleaner
+
+# Downloading fdroid 
+wget https://f-droid.org/F-Droid.apk
+
+# Installing fdroid
+adb install F-Droid.apk
+
+# Waiting whene apps are uninstalled
+sleep 60; echo "reboot"
+
+# Reboot device 
+adb reboot 
+
+# End of Script 
+echo done 
